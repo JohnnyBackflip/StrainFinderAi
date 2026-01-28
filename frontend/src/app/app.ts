c63bfe6d7740f752a9ae06537323f5caafcb6b08
JohnnyBackflip <johnnybackflip@gmail.com>
2026-01-28 17:40:51 +0100

    feat: Simplify frontend to a Hello World example

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit, AfterViewInit, inject } from '@angular/core';
-import { PreloaderService, SettingsService } from '@core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   template: `
-    <router-outlet />
+    <mat-toolbar color="primary">
+      <span>StrainBaseAi</span>
+    </mat-toolbar>
+    <div class="content">
+      <h1>Hello World!</h1>
+      <p>{{ backendMessage }}</p>
+    </div>
   `,
-  imports: [RouterOutlet],
+  styles: [`
+    .content {
+      padding: 24px;
+    }
+  `],
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule],
 })
-export class App implements OnInit, AfterViewInit {
-  private readonly preloader = inject(PreloaderService);
-  private readonly settings = inject(SettingsService);
+export class AppComponent {
+  private readonly http = inject(HttpClient);
+  backendMessage = '';
 
-  ngOnInit() {
-    this.settings.setDirection();
-    this.settings.setTheme();
-  }
-
-  ngAfterViewInit() {
-    this.preloader.hide();
+  constructor() {
+    this.http.get<{ message: string }>('/api/hello').subscribe(res => {
+      this.backendMessage = res.message;
+    });
   }
 }
